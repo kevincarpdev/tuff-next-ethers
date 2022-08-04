1bccbfbfa7f94773a82d026694e4962e25dd3717
Kevin Carpenter <carpenter.kevin@gmail.com>
2022-08-04 11:19:16 -0700

    Update Mission.js

--- component/Mission/Mission.js
+++ component/Mission/Mission.js
@@ -108,7 +108,7 @@ export default function Utility() {
               <VerticalTimelineElement
                 className={cn(s.timelineDiv, 'vertical-timeline-element--work')}
                 contentStyle={{ background: 'rgba(185,79,79,.6901960784313725)', color: '#fff' }}
-                date="25% Sold"
+                date="25%"
                 iconStyle={{ background: 'rgb(255,255,255)', color: '#fff' }}
               >
                 <h3 className="vertical-timeline-element-title">We launch exclusive giveaways for Tuff Guys holders.</h3>
@@ -116,7 +116,7 @@ export default function Utility() {
               <VerticalTimelineElement
                 className={cn(s.timelineDiv, 'vertical-timeline-element--work')}
                 contentStyle={{ background: 'rgba(185,79,79,.6901960784313725)', color: '#fff' }}
-                date="50% Sold"
+                date="50%"
                 iconStyle={{ background: 'rgb(255,255,255)', color: '#fff' }}
               >
                 <h3 className="vertical-timeline-element-title">We release limited merch for all the tuff guys (and gals) that own Tuff Guys.</h3>
@@ -124,7 +124,7 @@ export default function Utility() {
               <VerticalTimelineElement
                 className={cn(s.timelineDiv, 'vertical-timeline-element--work')}
                 contentStyle={{ background: 'rgba(185,79,79,.6901960784313725)', color: '#fff' }}
-                date="75% Sold"
+                date="75%"
                 iconStyle={{ background: 'rgb(255,255,255)', color: '#fff' }}
               >
                 <h3 className="vertical-timeline-element-title">Time for AIRDROPS!</h3>
@@ -135,7 +135,7 @@ export default function Utility() {
               <VerticalTimelineElement
                 className={cn(s.timelineDiv, 'vertical-timeline-element--work')}
                 contentStyle={{ background: 'rgba(185,79,79,.6901960784313725)', color: '#fff' }}
-                date="90% Sold"
+                date="90%"
                 iconStyle={{ background: 'rgb(255,255,255)', color: '#fff' }}
               >
                 <h3 className="vertical-timeline-element-title">As a treat for early supporters, not only will we ramp up existing giveaways for rare Tuff Guys, but we will begin to actively reward the most contributive and committed among the community.</h3>
@@ -143,14 +143,12 @@ export default function Utility() {
               <VerticalTimelineElement
                 className={cn(s.timelineDiv, 'vertical-timeline-element--work')}
                 contentStyle={{ background: 'rgba(185,79,79,.6901960784313725)', color: '#fff' }}
-                date="100% Sold"
-                iconStyle={{ background: 'rgb(255,255,255)', color: '#fff' }}
+                date="100%"
+                iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
               >
                 <h3 className="vertical-timeline-element-title">We begin to develop our COMIC STRIPS featuring your own tuff guys and using them to spread mental health awareness. These will be airdropped to every single Tuff Guy holder. This will kick off the 2nd generation of Tuff guys and our roadmap will keep up with MONTHLY updates.</h3>
               </VerticalTimelineElement>
-              <VerticalTimelineElement
-                iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-              />
+              
             </VerticalTimeline>
           </div>
         </div>
